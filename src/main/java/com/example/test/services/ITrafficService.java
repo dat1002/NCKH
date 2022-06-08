ff2f8367386dd51c64e28d362cb3684045ada406
Đạt <nvdat1002@gmail.com>
2022-06-08 10:37:10 +0700

    hellothe gioi

---
+++ src/main/java/com/example/test/services/ITrafficService.java
@@ -0,0 +1,21 @@
+package com.example.test.services;
+
+import com.example.test.dtos.TrafficDTO;
+import com.example.test.models.Traffic;
+import com.example.test.payload.AuthenticationRequest;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ITrafficService {
+    Traffic createNewTraffic(TrafficDTO trafficDTO, Integer idUser);
+    Traffic deleteTrafficById(Integer idUser,Integer idTraffic);
+    List<Traffic> getAllTrafficByIdUser(Integer idUser);
+    Set<Traffic> getAllTraffic();
+    Traffic editTrafficById(TrafficDTO trafficDTO, Integer idTraffic);
+    Traffic editAvatarTrafficById(MultipartFile file, Integer idTraffic);
+    Traffic getTrafficById(Integer idTraffic);
+
+
+}
